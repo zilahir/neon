ad0316b0e312f38937c3b90aea187f78ee5f463f
Richard Zilahi <richard.zilahi@salesframe.com>
2021-04-20 15:19:14 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+module.exports = {
+	devServer: {
+		writeToDisk: true,
+	},
+
+	entry: './src/index.js',
+	output: {
+			path: path.resolve('./dist/neon'),
+			filename: 'main.js'
+	},
+
+	module: {
+			rules: [
+					{ test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+					{
+						// look for .css or .scss files
+						test: /\.(css|scss)$/,
+						// in the `src` directory
+						use: [
+							{
+								loader: 'style-loader',
+							},
+							{
+								loader: 'css-loader',
+								options: {
+									importLoaders: 1,
+								},
+							},
+							{
+								loader: 'sass-loader',
+								options: {
+									sourceMap: true,
+								},
+							},
+						],
+					}
+			]
+	}
+};
