efa0770bc01dca9004102516b10d555c33fdcdd6
Richard Zilahi <richard.zilahi@salesframe.com>
2021-05-22 13:08:22 +0300

    implemented calling wp-admin using axios

--- src/components/FinalPrice/index.js
+++ src/components/FinalPrice/index.js
@@ -1,21 +1,47 @@
 import React, { useContext } from 'react'
 
+import { addCustomNeonToBasktet } from '../../utils/axios'
 import RootContext from '../../context/rootContext'
 import styles from './Price.module.scss'
 
 const FinalPrice = () => {
-  const { price, currentSize } = useContext(RootContext)
+  const {
+    price,
+    currentSize,
+    text,
+    activeColor,
+    activeFont,
+    backBoard,
+   } = useContext(RootContext)
+
+  function placeToBasket() {
+    addCustomNeonToBasktet({
+      text,
+      price,
+      currentSize,
+      activeColor,
+      activeFont,
+      backBoard,
+    })
+  }
   return (
-    <div className={styles.priceContainer}>
-      <p>
-        Végösszeg
-      </p>
-      <p>
-        {
-          `${price.find(p => p.size === currentSize.size).price} Ft`
-        }
-      </p>
-    </div>
+    <>
+      <div className={styles.priceContainer}>
+        <p>
+          Végösszeg
+        </p>
+        <p>
+          {
+            `${price.find(p => p.size === currentSize.size).price} Ft`
+          }
+        </p>
+      </div>
+      <div className={styles.placeToBasketBtnContainer}>
+        <button onClick={() => placeToBasket()} type="button">
+          Kosárba teszem
+        </button>
+      </div>
+    </>
   )
 }
 
--- src/utils/axios/index.js
+++ src/utils/axios/index.js
@@ -2,12 +2,15 @@ import axios from "axios";
 
 const axiosInstance = axios.create()
 
-export const createNewCustomNeon = newNeon => {
+export const addCustomNeonToBasktet = newNeon => {
+  const data = new FormData();
+  data.append('action', 'new_custom_neon_product');
+  data.append('newNeon', JSON.stringify(newNeon))
+  const params = new URLSearchParams(data);
+
   const ajaxUrl = window.ajaxUrl
   if (ajaxUrl) {
-    axiosInstance.post(ajaxUrl, {
-      ...newNeon
-    }).then(({ data }) => {
+    axiosInstance.post(ajaxUrl, data).then(({ data }) => {
       console.debug('result', data)
     })
   }
